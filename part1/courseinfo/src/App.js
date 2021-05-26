352329552c52dc8d0117bc1ff62504b3c0446443
Pablo Salas <pablosalas@outlook.com.ar>
2021-05-26 12:22:18 +0200

    ej 1.2 resolved

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header = ({course}) => <h1>{course}</h1>
 
-const Content = ({content}) => <p>{content}</p>
+const Part = ({part, exercises}) => <p>{part + ' ' + exercises}</p>
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+  return (
+      <div>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
+      </div>
+  )
+}
 
 const Total = ({total}) => <p>{total}</p>
 
@@ -18,9 +28,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content content={part1 + ' ' + exercises1}/>
-      <Content content={part2 + ' ' + exercises2}/>
-      <Content content={part3 + ' ' + exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
