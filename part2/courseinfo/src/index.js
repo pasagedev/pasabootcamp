f4434e4d731322ba6288425e296d22c539368e6c
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-01 12:24:59 +0200

    ej 2.1 completed

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <li>
+      {part.name} {part.exercises}
+    </li>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <ul>
+      {course.parts.map((part) =>
+        <Part key={part.name} part={part} />
+      )}
+    </ul>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
