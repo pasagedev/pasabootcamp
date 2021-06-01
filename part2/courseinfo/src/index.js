5eb39303d2affe8e1e04ab0b6d45e0e1eb36181c
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-01 12:28:30 +0200

    ej 2.2 completed

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -10,7 +10,7 @@ const Header = ({ course }) => {
 const Total = ({ course }) => {
   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
   return (
-    <p>Number of exercises {sum}</p>
+    <strong>Number of exercises {sum}</strong>
   )
 }
 
@@ -37,6 +37,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
