074eb0bb07b22b269eb91c3ea454f4fb005409b4
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-07 10:46:52 +0200

    weather request completed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -25,7 +25,14 @@ const Languages = ({ languages }) => (
     }
   </ul>
 )
-const CountryInformation = ({ country }) => (
+
+const CityWeather = ({ cityWeather }) => (
+  (cityWeather !== '' )
+  ? <div>{cityWeather.error.info}</div>
+  : <div>Not information to show</div>
+)
+
+const CountryDetail = ({ country, cityWeather }) => (
   <div>
     <h2>{country.name}</h2>
     <div>capital {country.capital}</div>
@@ -33,52 +40,79 @@ const CountryInformation = ({ country }) => (
     <h3>languages</h3>
     <Languages languages={country.languages} />
     <Flag country={country} />
+    <h3>wheather in {country.capital}</h3>
+    <CityWeather cityWeather={cityWeather} />
   </div>
 )
 
-const Countries = ({ countries, handleShowButton, countrySelected }) => {
-  if (countrySelected !=='')
-    return <CountryInformation country={countrySelected[0]} />
+const CountriesList = ({ countries, handleShowButton }) => (
+  countries.map((country) => (
+    <div key={country.numericCode}>
+      {country.name}
+      <button id={country.numericCode} onClick={handleShowButton}> show </button>
+    </div>
+  ))
+)
+
+const CountryInformation = ({ countries, handleShowButton, countrySelected, cityWeather }) => {
   if (countries.length > 10)
     return <div>To many matches, specify another filter</div>
-  else if (countries.length === 1)
-    return <CountryInformation country={countries[0]} />
+  else if (countrySelected !== '')
+    return <CountryDetail country={countrySelected} cityWeather={cityWeather} />
   else
-    return countries.map((country) => (
-      <div key={country.numericCode}>
-        {country.name}
-        <button id={country.numericCode} onClick={handleShowButton}> show </button>
-      </div>
-    ))
+    return <CountriesList countries={countries} handleShowButton={handleShowButton} />
 }
+
 
 const App = () => {
 
   const [countryFilter, setCountryFilter] = useState('')
   const [countries, setCountries] = useState([])
   const [countrySelected, setCountrySelected] = useState('')
+  const [cityWeather, setCityWeather] = useState('')
+
+  const requestWeather = (city) => {
+    const api_key = process.env.REACT_APP_API_KEY
+    const params = {
+      access_key: api_key,
+      query: city
+    }
+    axios.get(`http://api.weatherstack.com/current?`, { params })
+      .then(response => setCityWeather(response.data))
+  }
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all')
       .then(response => setCountries(response.data))
   }, [])
 
+  const countriesFiltered = countries.filter(({ name }) => (
+    name.toLowerCase().includes(countryFilter.toLowerCase())
+  ))
+
   const handleInputCountry = event => {
     setCountryFilter(event.target.value)
     setCountrySelected('')
   }
 
-  const showCountry = (event) => setCountrySelected(
-    countries.filter(country => country.numericCode === event.target.id))
+  const handleShowButton = (event) => setCountrySelected(
+    countriesFiltered.filter(country => country.numericCode === event.target.id)[0])
 
-  const filterCountries = countries.filter(({ name }) => (
-    name.toLowerCase().includes(countryFilter.toLowerCase())
-  ))
+  if (countriesFiltered.length === 1 && countrySelected === '')
+    setCountrySelected(countriesFiltered[0])
+
+  if (countrySelected !== '' && cityWeather === '')
+    requestWeather(countrySelected.city)
 
   return (
     <div>
       <CountrySearch inputValue={countryFilter} handleInput={handleInputCountry} />
-      <Countries countries={filterCountries} handleShowButton={showCountry} countrySelected={countrySelected} />
+      <CountryInformation
+        countries={countriesFiltered}
+        handleShowButton={handleShowButton}
+        countrySelected={countrySelected}
+        cityWeather={cityWeather}
+      />
     </div>
   )
 }
