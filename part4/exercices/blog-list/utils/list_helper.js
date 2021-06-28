c683e451a78d9478c0849566be603b2fac8bbba8
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-28 12:58:43 +0200

    ej 4.3-4.7 completed

--- part4/exercices/blog-list/utils/list_helper.js
+++ part4/exercices/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require('../models/blog')
+
 const dummy = blogs => 1
 
 const totalLikes = blogs => {
@@ -8,7 +10,7 @@ const totalLikes = blogs => {
 }
 
 const favoriteBlog = blogs => {
-  if (blogs.length === 0) { return {} }
+  if (blogs.length === 0) return {}
   const reducer = (mostLikedBlog, blog) => {
     return blog.likes >= mostLikedBlog.likes
       ? {
@@ -21,9 +23,49 @@ const favoriteBlog = blogs => {
 
   return blogs.reduce(reducer, { likes: 0 })
 }
+const mostBlogs = blogs => {
+  if (blogs.length === 0) return {}
+  let mostBlogsAuthor
+  const reducer = (blogsPerAuthor, blog) => {
+    const author = blog.author
+    if (!blogsPerAuthor[author]) {
+      blogsPerAuthor[author] = 1
+      mostBlogsAuthor = { author: author, blogs: blogsPerAuthor[author] }
+    } else {
+      blogsPerAuthor[author]++
+      if (blogsPerAuthor[author] > mostBlogsAuthor.blogs) {
+        mostBlogsAuthor = { author: author, blogs: blogsPerAuthor[author] }
+      }
+    }
+    return blogsPerAuthor
+  }
+  blogs.reduce(reducer, {})
+  return mostBlogsAuthor
+}
 
+const mostLikes = blogs => {
+  if (blogs.length === 0) return {}
+  let mostLikesBlog
+  const reducer = (likesPerAuthor, blog) => {
+    const author = blog.author
+    if (!likesPerAuthor[author]) {
+      likesPerAuthor[author] = blog.likes
+      mostLikesBlog = { author: author, likes: blog.likes }
+    } else {
+      likesPerAuthor[author] += blog.likes
+      if (likesPerAuthor[author] > mostLikesBlog.likes) {
+        mostLikesBlog = { author: author, likes: likesPerAuthor[author] }
+      }
+    }
+    return likesPerAuthor
+  }
+  blogs.reduce(reducer, {})
+  return mostLikesBlog
+}
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
