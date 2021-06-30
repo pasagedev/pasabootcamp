b8d6a555f607270422a58f6fa7ddc080c11a27e2
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-30 13:07:04 +0200

    ej 4.8 completed

--- part4/exercices/blog-list/controllers/blogs.js
+++ part4/exercices/blog-list/controllers/blogs.js
@@ -2,23 +2,20 @@ const blogsRouter = require('express').Router()
 
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => next(error))
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
 
-  module.exports = blogsRouter
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
---
+++ part4/exercices/blog-list/test/blog-api.test.js
@@ -0,0 +1,45 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/'
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  }
+
+]
+beforeAll(async () => {
+  await Blog.deleteMany({})
+
+  let blog = new Blog(initialBlogs[0])
+  await blog.save()
+
+  blog = new Blog(initialBlogs[1])
+  await blog.save()
+})
+
+test('blogs returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+test('all blogs are returned', async () => {
+  const result = await api.get('/api/blogs')
+
+  expect(result.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
