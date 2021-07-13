172828655f15335f768ab4465123c6f29f1f65a0
Pablo Salas <pablosalas@outlook.com.ar>
2021-07-13 13:40:35 +0200

    ej 5.7 completed

--- part5/exercises/bloglist-frontend/src/components/Blog.js
+++ part5/exercises/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const showWhenVisible = {display: visible ? '': 'none'}
+  const toggleVisivility = () => setVisible(!visible)
+  const buttonLabelVisibility = visible ? 'hide' : 'show'
+
+  return(
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisivility}>{buttonLabelVisibility}</button>
+        <div style={showWhenVisible}>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.user.username}</div>
+        </div>
+    </div>
+  )
+}
 
 export default Blog
