adc1af3bcf05743fae7753878ffff573a2326728
Pablo Salas <pablosalas@outlook.com.ar>
2021-07-21 11:55:35 +0200

    part 6.b completed

--- part6/notes-app-redux/src/app.js
+++ part6/notes-app-redux/src/app.js
@@ -1,10 +1,12 @@
 import NewNote from "./components/newNote"
 import Notes from './components/notes'
+import VisibilityFilter from "./components/VisibilityFilter"
 
 const App = () => {
     return(
       <div>
         <NewNote />
+        <VisibilityFilter />
         <Notes />
       </div>
     )
--- part6/notes-app-redux/src/index.js
+++ part6/notes-app-redux/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 import noteReducer from './reducers/noteReducer';
 import App from './app'
+import filterReducer from './reducers/filterReducer';
+import { filterChange } from './reducers/filterReducer';
+import { createNote } from './reducers/noteReducer';
 
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
 
-const store = createStore(noteReducer)
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+  )
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.render(
   <Provider store={store}>
