53eb0bf35a9aef4bd090f3fad48133ec2d058d73
Pablo Salas <pablosalas@outlook.com.ar>
2021-07-22 11:20:10 +0200

    part 6.c completed

--- part6/notes-app-redux/src/app.js
+++ part6/notes-app-redux/src/app.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 import NewNote from "./components/newNote"
 import Notes from './components/notes'
 import VisibilityFilter from "./components/VisibilityFilter"
+import { initializeNotes } from "./reducers/noteReducer"
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      dispatch(initializeNotes())
+  }, [dispatch])
+
+  
     return(
       <div>
         <NewNote />
