735b6106c2b439e68ffc9eace372f8f2fcabf03d
Pablo Salas <pablosalas@outlook.com.ar>
2021-07-24 18:36:44 +0200

    ej 7.5 completed

--- part7/exercises/routed-anecdotes/src/App.js
+++ part7/exercises/routed-anecdotes/src/App.js
@@ -90,6 +90,12 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const resetValues = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -106,7 +112,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='button' onClick={() => resetValues()}>reset</button>
       </form>
     </div>
   )
